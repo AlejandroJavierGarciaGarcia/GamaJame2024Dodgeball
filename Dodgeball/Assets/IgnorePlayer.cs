22781e22c165b6aab5403d6e761019aa911a2b58
Diego Lopez <111710248+jcdiegolopez@users.noreply.github.com>
2024-01-28 08:46:30 -0600

    camera

--- Dodgeball/Assets/IgnorePlayer.cs
+++ Dodgeball/Assets/IgnorePlayer.cs
@@ -7,6 +7,7 @@ public class ignoreLayerCollision : MonoBehaviour
     void Start()
     {
         Physics.IgnoreLayerCollision(6, 7);
+        Physics.IgnoreLayerCollision(1, 8);
         //Collision between two layers is ignored
         //In this example, Layer 6 & 7 are ignoring each other
     }
